44fb0e472543f4098893ae9fee0dc1bebaf9370b
bihanviranga <bihanviranga@gmail.com>
2024-04-21 00:50:06 +0530

    Function to scan directories for .git directories

--- scan.go
+++ scan.go
@@ -3,10 +3,61 @@ package main
 import (
   "flag"
   "fmt"
+  "strings"
+  "os"
+  "log"
 )
 
-func scan(path string) {
-  fmt.Println("scan %v", path)
+// Returns a list of subdirectories of 'directory' ending with '.git'.
+// The returned directories are the base directories of a repo, which means
+// they contain a '.git' directory.
+func scanGitDirectories(directories []string, directory string) []string {
+  // trim the trailing slash '/'
+  directory = strings.TrimSuffix(directory, "/")
+
+  f, err := os.Open(directory)
+  if err != nil {
+    log.Fatal(err)
+    // TODO quit here?
+  }
+
+  files, err := f.Readdir(-1)
+  f.Close()
+  if err != nil {
+    log.Fatal(err)
+    // TODO quit here?
+  }
+
+  var path string
+
+  for _, file := range files {
+    if file.IsDir() {
+      path = directory + "/" + file.Name()
+      if file.Name() == ".git" { // TODO move to const
+        path = strings.TrimSuffix(path, "/.git")
+        fmt.Println(path)
+        directories = append(directories, path)
+        continue
+      }
+      // TODO get the ignored files list from .gitignore, if present
+      if file.Name() == "node_modules" {
+        continue
+      }
+
+      directories = scanGitDirectories(directories, path)
+    }
+  }
+
+  return directories
+}
+
+// Scans a directory for git repositories
+func scan(directory string) {
+  fmt.Println("Directories found:")
+  repositories := recursiveScanDirectory(directory)
+  filePath := getDotfilePath()
+  addNewSliceElementsToFile(filePath, repositories)
+  fmt.Println("Successfully added")
 }
 
 func stats(email string) {
@@ -14,15 +65,15 @@ func stats(email string) {
 }
 
 func main() {
-  var folder string
+  var directory string
   var email string
 
-  flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repos")
+  flag.StringVar(&directory, "add", "", "add a directory to scan for Git repos")
   flag.StringVar(&email, "email", "your@email.com", "the email to scan")
   flag.Parse()
 
-  if folder != "" {
-    scan(folder)
+  if directory != "" {
+    scan(directory)
     return
   }
 
