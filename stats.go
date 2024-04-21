1749434c742cb3b0a7b34422c37d843ebdfde305
bihanviranga <bihanviranga@gmail.com>
2024-04-21 13:35:11 +0530

    Helper functions for printing info

--- stats.go
+++ stats.go
@@ -5,6 +5,8 @@ import (
   "gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
+type column []int
+
 const daysInLastSixMonths = 183
 const outOfRange = 99999
 
@@ -112,14 +114,72 @@ func processRepositories(email string) map[int]int {
   return commits
 }
 
-// Prints the commit stats.
+// Returns a slice of indexes of a map, ordered.
+func sortMapIntoSlice(m map[int]int) []int {
+  var keys []int
+  for k := range m {
+    keys = append(keys, k)
+  }
+  sort.Ints(keys)
+  return keys
+}
+
+// Generates a map with rows and columns with commit info
+func buildCols(keys []int, commits map[int]int) map[int]column {
+  cols := make(map[int]column)
+  col := column{}
+
+  for _, k := range keys {
+    week := int(k/7)    // 26, 25, .., 1
+    dayInWeek := k % 7  // 0, 1, 2, .., 6
+
+    if dayInWeek == 0 {
+      col = column{}
+    }
+
+    col = append(col, commits[k])
+
+    if dayInWeek == 6 {
+      cols[week] = col
+    }
+  }
+
+  return cols
+}
+
+// Prints the cells.
+func printCells(cols map[int]column) {
+  printMonths()
+  for j := 6; j >= 0; j-- {
+    for i := weeksInLastSixMonths + 1; i >= 0; i-- {
+      if i == weeksInLastSixMonths + 1 {
+        printDayCol(j)
+      }
+      if col, ok := cols[i]; ok {
+        if i == 0 && j == calcOffset() - 1 {
+          printCell(col[j], true)
+          continue
+        } else {
+          if len(col) > j {
+            printCell(col[j], false)
+            continue
+          }
+        }
+      }
+      printCell(0, false)
+    }
+    fmt.Printf("\n")
+  }
+}
+
+// Prepare the commit data for printing to the screen.
 func printCommitStats(commits map[int]int) {
   keys := sortMapIntoSlice(commits)
   cols := buildCols(keys, commits)
   printCells(cols)
 }
 
-// Calculates and prints stats.
+// Entry point for the stats calculation and printing.
 func stats(email string) {
   commits := processRepositories(email)
   printCommitStats(commits)
